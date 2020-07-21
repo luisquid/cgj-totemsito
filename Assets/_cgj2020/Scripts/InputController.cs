8360becccb13ad7482902a9cdab7bb9e25dad1a9
Squid <luisbazan417@gmail.com>
2020-07-21 02:48:47 -0500

    Mobile Dev. started
    - Rotation Script Reafactored
    - Mobile Swipes integrated

---
+++ Assets/_cgj2020/Scripts/InputController.cs
@@ -0,0 +1,75 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class InputController : MonoBehaviour
+{
+    public Vector2 startInput, endInput;
+
+    public RotateTotem rotateTot;
+
+    bool hasTouchReleased = true;
+
+    void Update()
+    {
+        if(Input.GetKeyDown(KeyCode.LeftArrow))
+        {
+            rotateTot.SetRotationParameters(0f, 1f);
+        }
+
+        if(Input.touchCount > 0)
+        {
+            Touch touch = Input.GetTouch(0);
+            if (touch.phase == TouchPhase.Began)
+            {
+                startInput = touch.position;
+                endInput = touch.position;
+            }
+
+            else if(touch.phase == TouchPhase.Ended)
+            {
+                if(hasTouchReleased)
+                {
+                    endInput = touch.position;
+                    CheckDirection();
+                }
+            }
+        }
+
+        rotateTot.UpdateTotem();
+    }
+
+    public void CheckDirection()
+    {
+        if (Mathf.Abs(startInput.x - endInput.x) > 20 && Mathf.Abs(startInput.x - endInput.x) > Mathf.Abs(startInput.y - endInput.y))
+        {
+            print("Horizontal Input");
+            if (startInput.x < endInput.x)
+            {
+                print("SWIPE RIGHT");
+                rotateTot.SetRotationParameters(0f, -1f);
+            }
+
+            else
+            {
+                print("SWIPE LEFT");
+                rotateTot.SetRotationParameters(0f, 1f);
+            }
+        }
+        else if (Mathf.Abs(startInput.y - endInput.y) > 20 && Mathf.Abs(startInput.y - endInput.y) > Mathf.Abs(startInput.x - endInput.x))
+        {
+            print("Vertical Input");
+            if (startInput.y < endInput.y)
+            {
+                print("SWIPE UP");
+                rotateTot.SetRotationParameters(1f, 0f);
+            }
+
+            else
+            {
+                print("SWIPE DOWN");
+                rotateTot.SetRotationParameters(-1f, 0f);
+            }
+        }
+    }
+}
---
+++ Assets/_cgj2020/Scripts/RotateTotem.cs
@@ -0,0 +1,54 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class RotateTotem : MonoBehaviour
+{
+    public float currentLerpTime;
+    public float lerpTime;
+
+    public GameObject deadParent;
+    Transform originalParent;
+
+    Quaternion originalRotation;
+    Quaternion targetRotation;
+    bool isRotating = false;
+
+    public void SetRotationParameters(float _vertical, float _horizontal)
+    {
+        isRotating = true;
+        currentLerpTime = 0f;
+        originalParent = transform.parent;
+
+        deadParent.transform.rotation = Quaternion.identity;
+        deadParent.transform.position = transform.position;
+        transform.parent = deadParent.transform;
+
+        originalRotation = deadParent.transform.rotation;
+        targetRotation = originalRotation;
+
+        targetRotation *= Quaternion.Euler(new Vector3(_vertical, _horizontal, 0f) * 90f);
+    }
+
+    public void UpdateTotem()
+    {
+        if (isRotating)
+        {
+            if (currentLerpTime < lerpTime)
+            {
+                currentLerpTime += Time.deltaTime;
+                float perc = currentLerpTime / lerpTime;
+
+                deadParent.transform.rotation = Quaternion.Slerp(originalRotation, targetRotation, perc);
+            }
+
+            else
+            {
+                deadParent.transform.rotation = targetRotation;
+                transform.parent = originalParent;
+
+                isRotating = false;
+            }
+        }
+    }
+}
